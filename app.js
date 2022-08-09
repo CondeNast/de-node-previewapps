daf5e80e191ecab497ce22e882f5465d78b75517
mshopsin <michael_shopsin@condenast.com>
2022-08-09 11:29:49 -0400

    Download files

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 //var usersRouter = require('./routes/users');
 var cnPreviewRouter = require('./routes/cnpreview');
 var cnPasswordRouter = require('./routes/cnpassword');
+var downloadRouter = require('./routes/download');
 
 var app = express();
 
@@ -23,8 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 //app.use('/users', usersRouter);
-app.use('/cnpreview/vanityfair', cnPreviewRouter);
+app.use('/cnpreview/*', cnPreviewRouter);
 app.use('/posts', cnPasswordRouter);
+app.use('/previewapps/*', downloadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
