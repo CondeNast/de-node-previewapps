a655ac3251febb174acae8a7a2e3bd3da50377cc
mshopsin <michael_shopsin@condenast.com>
2022-08-11 12:51:40 -0400

    Downloads are filtered for valid paths and replaces strings in the manifest.plist

--- routes/download.js
+++ routes/download.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     const filename = '.' + req.baseUrl;
-    res.download(filename);
+    if(filename.match(/^\.\/previewapps\/\w*\/(\w*)?manifest.plist/)) {
+        fs.readFile(filename,"utf8", function(err, data) {
+            var newData = data.replaceAll("https://stagpublish.condenast.com:3001","http://localhost:3000");
+            res.setHeader('Content-Type', 'application/plist');
+            res.end(newData);
+        });
+    } else if(filename.match(/^\.\/previewapps\/\w*\/\w*\.\w{1,4}/)) {
+        res.download(filename);
+    } else {
+        res.render('error', {message: 'Bad filename', error: {status: 'Path error', stack: 'Filename is incorrect'}})
+    }
 });
 
 module.exports = router;
