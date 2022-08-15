3f51e93e5d700058e8d62c56603be29676a25967
mshopsin <michael_shopsin@condenast.com>
2022-08-15 13:52:37 -0400

    Removed https as we're going to be port mapped to 443 or 3000 instead of hosting https ourselves. Server set in cnserver.js is not final as Devops is still deciding what to give us.

--- util/cnserver.js
+++ util/cnserver.js
@@ -1,7 +1,7 @@
 exports.serverUrl = function(req) {
     var server = 'http://localhost:3000';
     if(req.app.get('env') === 'production') {
-        server = 'https://cnstud.io:3001';
+        server = 'https://conde.io:3000';
     } else if(req.app.get('env') === 'staging') {
         server = 'https://stagpublish.condenast.com:3001';
     }
